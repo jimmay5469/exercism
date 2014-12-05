100ca6c139cc6f93cadb0190254d52aa798926d5
jimmay5469 <jimmay5469@gmail.com>
2014-12-04 23:46:47 -0500

    Bob cleanup round 1

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,11 +1,23 @@
 var Bob = function() {};
 
+Bob.prototype.isYell = function(input) {
+  return input.toUpperCase() == input && input.toLowerCase() != input;
+}
+
+Bob.prototype.isQuestion = function(input) {
+  return input[input.length-1] == '?';
+}
+
+Bob.prototype.isNothing = function(input) {
+  return !input.trim();
+}
+
 Bob.prototype.hey = function(input) {
-  if (input.toUpperCase() == input && input.toLowerCase() != input) {
+  if (this.isYell(input)) {
     return 'Whoa, chill out!'
-  } else if (input[input.length-1] == '?') {
+  } else if (this.isQuestion(input)) {
     return 'Sure.'
-  } else if (!input.trim()) {
+  } else if (this.isNothing(input)) {
     return 'Fine. Be that way!';
   }
   return 'Whatever.';
