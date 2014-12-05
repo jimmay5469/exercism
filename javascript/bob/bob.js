d2f4fef1f2c882bec9f8aee6218862dec74cc4c3
jimmay5469 <jimmay5469@gmail.com>
2014-12-04 23:40:46 -0500

    Getting js bob tests passing

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,6 +1,13 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
+  if (input.toUpperCase() == input && input.toLowerCase() != input) {
+    return 'Whoa, chill out!'
+  } else if (input[input.length-1] == '?') {
+    return 'Sure.'
+  } else if (!input.trim()) {
+    return 'Fine. Be that way!';
+  }
   return 'Whatever.';
 };
 
