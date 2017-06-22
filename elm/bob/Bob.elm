d2e5ed63feabc0334d5c9b541aaa3c2891464060
jimmay5469 <jimmay5469@gmail.com>
2017-06-22 15:31:04 -0400

    Elm bob solution

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,1 +1,11 @@
 module Bob exposing (..)
+
+hey comment = 
+  if String.trim comment == "" then
+    "Fine. Be that way!"
+  else if String.toUpper comment == comment && String.toLower comment /= comment then
+    "Whoa, chill out!"
+  else if String.endsWith "?" comment then
+    "Sure."
+  else
+    "Whatever."
