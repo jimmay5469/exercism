47bf90b148c61951d750c45906bcf3b9adeb670d
jimmay5469 <jimmay5469@gmail.com>
2017-06-23 16:30:01 -0400

    Elm pangram solution

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -1,1 +1,25 @@
 module Pangram exposing (..)
+
+import String exposing (toLower, concat)
+import List exposing (map, sort)
+import Regex exposing (regex, find, HowMany(All))
+import Set
+
+findLetters : String -> List String
+findLetters l = l |> find All (regex "[a-z]") |> map .match
+
+unique : List String -> List String
+unique l = l |> Set.fromList |> Set.toList
+
+isPangram : String -> Bool
+isPangram sentence =
+  let 
+    sortedUniqueLowercasedLetters =
+      sentence
+        |> toLower
+        |> findLetters
+        |> unique
+        |> sort
+        |> concat
+  in
+    sortedUniqueLowercasedLetters == "abcdefghijklmnopqrstuvwxyz"
