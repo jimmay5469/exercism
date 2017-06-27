3592158acc38fd03d0f2e720d0b6eed36eab0d01
jimmay5469 <jimmay5469@gmail.com>
2017-06-27 16:10:47 -0400

    Elm accumulate solution

--- elm/accumulate/Accumulate.elm
+++ elm/accumulate/Accumulate.elm
@@ -1,1 +1,10 @@
 module Accumulate exposing (..)
+
+accumulate : (a -> b) -> List a -> List b
+accumulate fn list =
+    case list of
+        [] -> []
+        ( head :: tail ) ->
+            let headResult = fn head
+                tailResult = accumulate fn tail
+            in headResult :: tailResult
